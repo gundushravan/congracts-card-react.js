62a0227d76a58cf0b28db77de0b679f6ef2b81df
shravankumar <gundusravan123456@gmail.com>
2023-01-25 09:48:36 +0000

    congracts card

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div className="container">
+    <h1>Congratulations</h1>
+    <div class="inside-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h2>Kiran V</h2>
+      <p>Vishnu Institute of Computer and Technology Bhimavaram</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"></img>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
